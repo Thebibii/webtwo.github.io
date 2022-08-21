3819177bc1d36bcd7ebc4583bb2c9121dd3412bb
habibiesanji <habibiesanji@gmail.com>
2022-08-22 00:11:14 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+const menuBar = document.querySelector(".ri-menu-3-line");
+const menuNav = document.querySelector(".menu");
+const bgNav = document.querySelector(".bgnavbar");
+
+menuBar.addEventListener("click", function (){
+	menuNav.classList.toggle("menuactive");
+	bgNav.classList.toggle("bgactive");
+});
+
+bgNav.addEventListener("click", function (){
+	menuNav.classList.remove("menuactive");
+	bgNav.classList.remove("bgactive");
+})
